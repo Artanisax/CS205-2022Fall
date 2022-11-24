8668089c2d9d608136d0d5a3ba9e5a9c7da6c757
“Artanisax” <1415197174@qq.com>
2022-11-24 10:44:28 +0800

    slight modification

--- Project4/src/Matrix.c
+++ Project4/src/Matrix.c
@@ -2,20 +2,18 @@
 #include <stdio.h>
 #include <immintrin.h>
 
-Matrix *createMatrix(const size_t row, const size_t col, const entry_t *const entry) {
+Matrix *createMatrix(const size_t row, const size_t col, const float *const entry) {
     Matrix *mat = (Matrix *)malloc(sizeof(Matrix));
     mat->row = row;
     mat->col = col;
     size_t siz = row*col;
-    mat->entry = (entry_t *)malloc(sizeof(entry_t)*siz);
-    for (size_t i = 0; i < siz; ++i)
-        mat->entry[i] = entry[i];
+    mat->entry = (float *)malloc(sizeof(float)*siz);
+    memcpy(mat->entry, entry, sizeof(float)*siz);
     MPL_push_front(mat);
     return mat;
 }
 
 void deleteMatrix(Matrix *const mat) {
-    MPL_erase(mat);
     free(mat->entry);
     free(mat);
 }
@@ -24,17 +22,13 @@ void copyMatrix(Matrix *const a, const Matrix *const b) {
     a->row = b->row;
     a->col = b->col;
     size_t siz = b->row*b->col;
-    a->entry = (entry_t *)realloc(a->entry, sizeof(entry_t)*siz);
-    memcpy(a->entry, b->entry, sizeof(entry_t)*siz);
+    a->entry = (float *)realloc(a->entry, sizeof(float)*siz);
+    memcpy(a->entry, b->entry, sizeof(float)*siz);
 }
 
 Matrix *addMatrix(const Matrix *const a, const Matrix *const b) {
-    if (a->row != b->row || a->col != b->col) {
-        puts("Error in addMatrix(): Inequal dementions!");
-        return NULL;
-    }
     size_t size = a->row*a->col;
-    entry_t entry[size];
+    float entry[size];
     for (size_t i = 0; i < size; ++i)
         entry[i] = a->entry[i]+b->entry[i];
     return createMatrix(a->row, a->col, entry);
@@ -42,7 +36,7 @@ Matrix *addMatrix(const Matrix *const a, const Matrix *const b) {
 
 Matrix *substractMatrix(const Matrix *const a, const Matrix *const b) {
     size_t size = a->row*a->col;
-    entry_t entry[size];
+    float entry[size];
     for (size_t i = 0; i < size; ++i)
         entry[i] = a->entry[i]-b->entry[i];
     return createMatrix(a->row, a->col, entry);
@@ -50,7 +44,7 @@ Matrix *substractMatrix(const Matrix *const a, const Matrix *const b) {
 
 Matrix *originalMultiplyMatrix(const Matrix *const a, const Matrix *const b) {
     size_t size = a->row*b->col;
-    entry_t entry[size];
+    float entry[size];
     for (size_t i = 0; i < a->row; ++i)
         for (size_t j = 0; j < b->col; ++j) {
             entry[i*b->col+j] = 0.0;
@@ -68,7 +62,7 @@ void printMatrix(const Matrix *const mat) {
     printf("row = %zu, col = %zu\n", mat->row, mat->col);
     for (size_t i = 0; i < mat->row; ++i) {
         for (size_t j = 0; j < mat->col; ++j)
-            printf(entry_place_holder, mat->entry[i*mat->col+j]);
+            printf("%.2f\t", mat->entry[i*mat->col+j]);
         putchar('\n');
     }
 }
