0cabb7fdc56f030c8fe37479bba947c17a9bc723
“Artanisax” <1415197174@qq.com>
2022-12-17 19:33:34 +0800

    reset project

--- Project5/include/Matrix.hpp
+++ Project5/include/Matrix.hpp
@@ -11,29 +11,27 @@ using std::make_shared;
 using std::default_delete;
 using std::string;
 using std::ostream;
+using std::cout;
+using std::cerr;
+using std::endl;
 
 template <typename T>
 class Matrix
 {
 private:
-	size_t channel, row, col;
+	size_t channel, row, col, roi, r, c;
 	shared_ptr<T> entry;
 
-	static void hard_copy(T *dest, const T *src, const size_t siz);
-
-	static void set_zero(T *p, size_t siz);
-
 public:
-	Matrix(const size_t channel, const size_t row, const size_t col, const T *entry):
-		channel(channel), row(row), col(col), entry(new T[row*col*channel], default_delete<T[]>())
-	{ if (entry)  hard_copy(this->entry.get(), entry, row*col*channel); }
+	Matrix(const size_t channel, const size_t row, const size_t col, const T *entry);
 
-	Matrix(const Matrix &mat): row(mat.row), col(mat.col), entry(mat.entry) {}
+	Matrix(const Matrix &mat);
 
-	T get(size_t k, size_t i, size_t j) const
-	{ return entry.get()[k*row*col+i*col+j]; }
+	T get(size_t k, size_t i, size_t j) const;
 
 	string to_string() const;
+
+	void print() const;
 
 	void uniquify();
 
@@ -86,216 +84,25 @@ public:
 };
 
 template <typename T>
-void Matrix<T>::hard_copy(T *dest, const T *src, const size_t siz)
+Matrix<T>::Matrix(const size_t channel, const size_t row, const size_t col, const T *entry):
+	channel(channel), row(row), col(col), r(row), c(col), entry(new T[channel*row*col], default_delete<T[]>())
 {
-    if (typeid(T) == typeid(short) || typeid(T) == typeid(int) ||
-		typeid(T) == typeid(long long) || typeid(T) == typeid(float) ||
-		typeid(T) == typeid(double))
-		memcpy(dest, src, siz*sizeof(T));
-	else
-		for (size_t i = 0; i < siz; ++i)
-			dest[i] = src[i];
+	T *p = this->entry.get();
+	if (entry)  memcpy(p, entry, channel*row*col*sizeof(T));
+	roi = 0;
 }
 
 template <typename T>
-void Matrix<T>::set_zero(T *p, const size_t siz)
-{
-    if (typeid(T) == typeid(short) || typeid(T) == typeid(int) ||
-		typeid(T) == typeid(long long) || typeid(T) == typeid(float) ||
-		typeid(T) == typeid(double))
-		bzero(p, siz*sizeof(T));
-	else
-		for (size_t i = 0; i < siz; ++i)
-			dest[i] = 0;
-}
+Matrix<T>::Matrix(const Matrix<T> &mat): channel(mat.channel), row(mat.row), col(mat.col),
+	roi(mat.roi), r(mat.r), c(mat.c), entry(mat.entry) {}
 
 template <typename T>
-string Matrix<T>::to_string() const
+T Matrix<T>::get(const size_t k, const size_t i, const size_t j) const
 {
-    string s = "\n";
-    T *p = entry.get();
-    size_t area = row*col;
-    for (size_t i = 0; i < channel; ++i)
-    {
-        for (size_t j = 0, head_row = i*area; j < row; ++j)
-        {
-            for (size_t k = 0, head_col = head_row+j*col; k < col; ++k)
-                s += std::to_string(p[head_col+k])+" ";
-            s += "\n";
-        }
-        s += "\n";
-    }
-    return s;
-}
-
-template <typename T>
-void Matrix<T>::uniquify()
-{
-    if (entry.unique())  return;
-    T *src = entry.get();
-    entry = shared_ptr<T>(new T[row*col*channel], default_delete<T[]>());
-    hard_copy(entry.get(), src, this->channel*this->row*this->col);
-}
-
-template <typename T>
-bool Matrix<T>::operator==(const Matrix &mat) const
-{
-    if (channel != mat.channel || row != mat.row || col != mat.col)
-        return false;
-    if (entry.get() == mat.entry.get())
-        return true;
-    size_t siz = channel*row*col;
-    T *p[2] = {entry.get(), mat.entry.get()};
-    for (size_t i = 0; i < siz; ++i)
-        if (p[0][i] != p[1][i])  return false;
-    return true;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator=(const Matrix &mat)
-{
-    uniquify();
-    channel = mat.channel;
-    row = mat.row;
-    col = mat.col;
-    entry = mat.entry;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator+(const Matrix &mat) const
-{
-    Matrix<T> res(this->channel, this->row, this->col, nullptr);
-    size_t siz = this->channel*this->row*this->col;
-    T *dest = res.entry.get(), *src[2] = {this->entry.get(), mat.entry.get()};
-    for (size_t i = 0; i < siz; ++i)
-        dest[i] = src[0][i]+src[1][i];
-    return res;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator-(const Matrix &mat) const
-{
-    Matrix<T> res(channel, row, col, nullptr);
-    size_t siz = channel*row*col;
-    T *dest = res.entry.get(), *src[2] = {entry.get(), mat.entry.get()};
-    for (size_t i = 0; i < siz; ++i)
-        dest[i] = src[0][i]-src[1][i];
-    return res;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator*(const Matrix &mat) const
-{
-    Matrix<T> res(channel, row, mat.col, nullptr);
-    T *dest = res.entry.get(), *src[2] = {entry.get(), mat.entry.get()};
-    set_zero(dest, channel*row*mat.col);
-    for (size_t t = 0, area[3] = {row*col, mat.row*mat.col, row*mat.col};          t < channel; ++t)
-    for (size_t i = 0, head_row[3] = {t*area[0], t*area[1], t*area[2]};            i < row;     ++i)
-    for (size_t k = 0, head_i = head_row[0]+i*col, head_p = head_row[2]+i*mat.col; k < col;     ++k)
-    for (size_t j = 0, ik = head_i+k, head_k = head_row[1]+k*mat.col;              j < mat.col; ++j)
-        dest[head_p+j] += src[0][ik]*src[1][head_k+j];
-	return res;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator+(const T &x) const
-{
-    Matrix<T> res(channel, row, col, nullptr);
-    T *dest = res.entry.get(), *src = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        dest[i] = src[i]+x;
-    return res;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator-(const T &x) const
-{
-    Matrix<T> res(channel, row, col, nullptr);
-    T *dest = res.entry.get(), *src = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        dest[i] = src[i]-x;
-    return res;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator*(const T &x) const
-{
-    Matrix<T> res(channel, row, col, nullptr);
-    T *dest = res.entry.get(), *src = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        dest[i] = src[i]*x;
-    return res;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator/(const T &x) const
-{
-    Matrix<T> res(channel, row, col, nullptr);
-    T *dest = res.entry.get(), *src = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        dest[i] = src[i]/x;
-    return res;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator+=(const Matrix &mat)
-{
-    uniquify();
-    T *dest = entry.get(), *src = mat.entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        dest[i] += src[i];
-    return *this;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator-=(const Matrix &mat)
-{
-    uniquify();
-    T *dest = entry.get(), *src = mat.entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        dest[i] -= src[i];
-    return *this;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator*=(const Matrix &mat)
-{
-    *this = (*this)*mat;
-    return *this;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator+=(const T &x)
-{
-    T *p = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        p[i] += x;
-    return *this;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator-=(const T &x)
-{
-    T *p = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        p[i] -= x;
-    return *this;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator*=(const T &x)
-{
-    T *p = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        p[i] *= x;
-    return *this;
-}
-
-template <typename T>
-Matrix<T> &Matrix<T>::operator/=(const T &x)
-{
-    T *p = entry.get();
-    for (size_t i = 0, siz = channel*row*col; i < siz; ++i)
-        p[i] /= x;
-    return *this;
+	if (i >= r || j >= c)
+	{
+		cerr << "\nError: Out of Range in " << __func__ << endl;
+		return 0;
+	}
+	return entry.get()[k*row*col+roi+i*col+j];
 }
--- Project5/src/demo.cpp
+++ Project5/src/demo.cpp
@@ -1,13 +1,15 @@
 #include "Matrix.hpp"
 
-using std::cout;
-
 int main () {
-    double *e[2] = {new double[4], new double[4]};
-    e[0][0] = 0, e[0][1] = 1, e[0][2] = 2, e[0][3] = 3;
-    e[1][0] = 2, e[1][1] = 3, e[1][2] = 1, e[1][3] = 0;
-    Matrix<double> m0(2, 1, 2, e[0]);
-    Matrix<double> m1(2, 2, 1, e[1]);
-    cout << m0*m1;
+    // double *e[2] = {new double[4], new double[4]};
+    // e[0][0] = 0, e[0][1] = 1, e[0][2] = 2, e[0][3] = 3;
+    // e[1][0] = 2, e[1][1] = 3, e[1][2] = 1, e[1][3] = 0;
+    // Matrix<double> m0(2, 1, 2, e[0]);
+    // Matrix<double> m1(2, 2, 1, e[1]);
+    // cout << m0*m1;
+    double *entry = new double;
+    *entry = 1;
+    Matrix<double> mat(1, 1, 1, entry);
+    cout << mat.get(0,0,0) << ' ' << mat.get(0,0,1) << '\n';
     return 0;
 }
